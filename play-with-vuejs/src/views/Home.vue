433217093b9df92a500892305dd445ad55f06b1b
setiawanswan <setiawan.tumilin@gmail.com>
2019-07-15 21:51:17 +0700

    loop in vuejs

--- play-with-vuejs/src/views/Home.vue
+++ play-with-vuejs/src/views/Home.vue
@@ -1,20 +1,49 @@
 <template>
-  <div id="app">
-    Number {{MixNumberOne}} - Number {{MixNumberTwo}}
+  <div>
+    <div id="app">Number {{MixNumberOne}} - Number {{MixNumberTwo}}</div>
+    <div id="member">
+      <ul>
+        <li 
+          v-for="member in members"
+          :key="member"
+        >
+          {{member.nama}} {{member.umur}}
+        </li>
+      </ul>
+    </div>
+    <div id="crew">
+      <ul>
+        <li 
+          v-for="(key, crew) in crews"
+          :key="crew"
+        >
+        {{crew}} - {{key}}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  data(){
-    return{
-      MixNumberOne : 2,
-    }
+  data() {
+    return {
+      MixNumberOne: 2,
+      members : [
+        {nama: "Ardhim", umur: 10},
+        {nama: "Kresna", umur: 11},
+        {nama: "Hasyim", umur: 12}
+      ],
+      crews : {
+        nama : "Sultan", 
+        umur : 12,
+        hobi : "mancing"
+      }
+    };
   },
   computed: {
-    MixNumberTwo : function(){
-      return this.MixNumberOne + 2
+    MixNumberTwo: function() {
+      return this.MixNumberOne + 2;
     }
   }
 };
